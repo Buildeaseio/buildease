bc8e94c1b8ee0d65931c251b54ad6e1c4b3c9e6e
Gabriel Gratton <realgabrielgratton@gmail.com>
2025-02-25 15:14:24 -0500

    Update Hero.tsx
    Fix Construction Management Platform Badge

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
+import Link from 'next/link'
 import { useState } from 'react'
-import { RiArrowRightUpLine } from "react-icons/ri"
 import { FadeContainer, FadeDiv, FadeSpan } from "../Fade"
 import DemoForm from './DemoForm'
 import GameOfLife from "./HeroBackground"
@@ -13,12 +13,10 @@ export default function Hero() {
     <section aria-label="hero">
       <FadeContainer className="relative flex flex-col items-center justify-center">
         <FadeDiv className="mx-auto">
-          <a
-            aria-label="View latest update the changelog page"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-auto w-full"
+          <Link
+            href="/"
+            aria-label="View latest update"
+            className="mx-auto w-full relative z-20 block"
           >
             <div className="inline-flex max-w-full items-center gap-3 rounded-full bg-white/5 px-2.5 py-0.5 pr-3 pl-0.5 font-medium text-gray-900 ring-1 shadow-lg shadow-[#2286b9]/20 ring-[#2286b9]/10 filter backdrop-blur-[1px] transition-colors hover:bg-[#2286b9]/[2.5%] focus:outline-hidden sm:text-sm">
               <span className="shrink-0 truncate rounded-full border bg-gray-50 px-2.5 py-1 text-sm text-gray-600 sm:text-xs">
@@ -28,11 +26,9 @@ export default function Hero() {
                 <span className="w-full truncate">
                   Construction Management Platform
                 </span>
-
-                <RiArrowRightUpLine className="size-4 shrink-0 text-gray-700" />
               </span>
             </div>
-          </a>
+          </Link>
         </FadeDiv>
         <h1 className="mt-8 text-center text-5xl font-semibold tracking-tighter text-gray-900 sm:text-8xl sm:leading-[5.5rem]">
           <FadeSpan>Autonomy</FadeSpan> <FadeSpan>for</FadeSpan>
